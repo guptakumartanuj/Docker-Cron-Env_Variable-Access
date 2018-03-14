daa01eaaf72beb9adcdf10d1ea01e2df934bbc07
tangupta <tangupta@adobe.com>
2018-03-14 15:05:04 +0530

    Made change into the entry file

--- run-crond.sh
+++ run-crond.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 #crontab crontab
 crontab  /tmp/crontab
-// THe below line shows how to grep environment variable aur use them in the script. In this, first I have greped the env variable (ENV_DAG_CONTAINER) and then moved this variable into the temp file. Finally added this at the top of the script so that we can use it.
+#The below line shows how to grep environment variable aur use them in the script. In this, first I have greped the env variable (ENV_DAG_CONTAINER) and then moved this variable into the temp file. Finally added this at the top of the script so that we can use it.
 env | egrep '^ENV_DAG' | cat - /dag_script.sh > temp && mv temp /dag_script.sh
 
-// To start the cron service inside the container
+#To start the cron service inside the container. If below is not working then use "cron/crond -L /var/log/cron.log"
 service cron start
